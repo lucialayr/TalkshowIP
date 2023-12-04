11d9742a1dc0519aeb8fd5fc6e876a78318a4204
lucialayr <lucialayr@Lucias-MacBook-Pro.local>
2023-12-03 17:55:04 -0800

    layout lokal fertig

--- code/shiny/app.R
+++ code/shiny/app.R
@@ -8,6 +8,7 @@ library(plotly)
 
 df = read_csv("data/Daten.csv")
 
+colorscheme = "lapaz"
 
 cutoff = function(df, variable, thresh = 1) {
   df0 = df %>%
@@ -53,7 +54,7 @@ plot1 = function(variable, df=df, thresh = 1) {
   p1 = ggplot() + theme_void() +
       geom_bar(data = df1, alpha = 1, color = "black", stat = "identity", width = .5,
                aes(x = indicator, y =n,  fill = indicator, text = text)) +
-      scico::scale_fill_scico_d(palette = "batlow", direction = 1) +
+      scico::scale_fill_scico_d(palette = colorscheme, direction = 1, end = .85, begin = .3) +
     scale_y_continuous(expand = c(0,0)) +
     scale_x_discrete(name = "") + 
     add_plot_layout(fontsize = 15) +
@@ -105,7 +106,7 @@ plot2 = function(variable1, variable2, df, thresh = 2) {
     geom_point(data = df2, shape = 21,  position = position_jitter(width = .4, height = .4, seed = 123),  alpha = .9,
                  aes(x = !!rlang::sym(variable1), y = !!rlang::sym(variable2),  fill = !!rlang::sym(variable1), size = n, text = Name)) +
     scale_size_continuous(breaks = c(0, 1, 2, 3, 4), limits = c(0,max(df2$n))) +
-    scico::scale_fill_scico_d(palette = "batlow", direction = 1) +
+    scico::scale_fill_scico_d(palette = colorscheme, direction = 1, end = .85, begin = .3) +
     scale_x_discrete(expand = c(0,0), name = variable1) +
     scale_y_discrete(expand = c(0,0), name = variable2) +
     add_plot_layout(fontsize = 15) +
@@ -117,11 +118,13 @@ plot2 = function(variable1, variable2, df, thresh = 2) {
   return(p2)
 }
 
-plot3 = function(start = "2023-10-07", end = "2023-10-30", df) {
+plot3 = function(start = "2023-10-07", end = "2023-11-30", df, variable, thresh = 2) {
   
   print(start)
+  # area plot needs a second dataframe, where everz guest is 1/no guests in that specific show so that y axis is the same
+  # dots should give info of shows and when and which guests.
   
-  df = df %>%
+  df_date = df %>%
     mutate(Datum = as.Date(Datum, format = "%d.%m.%y")) %>%
     filter(Datum >= as.Date(start), Datum <= as.Date(end)) %>%
     mutate(Week = lubridate::week(Datum),
@@ -131,23 +134,66 @@ plot3 = function(start = "2023-10-07", end = "2023-10-30", df) {
     group_by(Week, Sunday) %>%
     count()
   
+  df01 = df %>%
+    mutate(Datum = as.Date(Datum, format = "%d.%m.%y")) %>%
+    filter(Datum >= as.Date(start), Datum <= as.Date(end)) %>%
+    mutate(Week = lubridate::week(Datum),
+           Sunday = Datum - lubridate::wday(Datum) + 1) %>%
+    group_by(!!rlang::sym(variable), Week) %>%
+    count() %>%
+    mutate(indicator = case_when(!!rlang::sym(variable) %in% c("deutsch", "israelisch", 
+                                                               "jüdisch", "palestinänsisch") ~ !!rlang::sym(variable),
+                                 n > thresh ~ !!rlang::sym(variable),
+                                 TRUE ~ "andere")) 
+  
+  df_area = df %>%
+    mutate(Datum = as.Date(Datum, format = "%d.%m.%y")) %>%
+    filter(Datum >= as.Date(start), Datum <= as.Date(end)) %>%
+    mutate(Week = lubridate::week(Datum),
+           Sunday = Datum - lubridate::wday(Datum) + 1) %>%
+    select(Week, Datum, Sunday, Name, !!rlang::sym(variable)) %>%
+    unique() %>%
+    group_by(Week, Sunday, !!rlang::sym(variable)) %>%
+    count(name = "variable_per_week") %>%
+    mutate(indicator = case_when(!!rlang::sym(variable) %in% c("deutsch", "israelisch", 
+                                                               "jüdisch", "palestinänsisch",
+                                                               unique(df$Profession)) ~ !!rlang::sym(variable),
+                                 variable_per_week > thresh ~ !!rlang::sym(variable),
+                                 TRUE ~ "andere")) %>%
+    ungroup() %>%
+    group_by(Week, Sunday, indicator) %>%
+    mutate(variable_per_week = sum(variable_per_week)) %>%
+    select(Week, Sunday, variable_per_week, indicator) %>%
+    unique() %>%
+    group_by(Week) %>%
+    mutate(share = variable_per_week/sum(variable_per_week)) %>%
+    left_join(df_date) %>%
+    mutate(share = share*n) %>%
+    ungroup()
+  
+  all_comb = df_area %>%
+    expand(Sunday, indicator) %>%
+    left_join(df_area) %>%
+    mutate(share = replace_na(share, 0))
+    
   ggplot() + theme_void() +
-    geom_line(data = df, aes(x = Sunday + 1, y = n)) +
-    geom_point(data = df, fill = "grey50", color = "black", shape = 21, position = "identity",
-               aes(x = Sunday + 1, y = n, size = n)) +
+    geom_area(data = all_comb, aes(x = Sunday + 1, y = share, fill = indicator), color = "black", alpha = 1) +
+    geom_line(data = df_date, aes(x = Sunday + 1, y = n)) +
+    #geom_point(data = df_date, fill = "grey50", color = "black", shape = 21, position = "identity",
+    #           aes(x = Sunday + 1, y = n, size = n)) +
     add_plot_layout(fontsize = 15) +
-    scale_y_continuous(limits = c(0, max(df$n)), expand = c(0,.5)) +
-    scale_size_continuous(limits = c(1, max(df$n)), range = c(1, max(df$n)*2)) +
+    scale_x_date(name = "Wochenbeginn (Sonntag)") +
+    scale_y_continuous(limits = c(0, max(df_date$n)), expand = c(0,.5)) +
+    scale_size_continuous(limits = c(1, max(df_date$n)), range = c(1, max(df_date$n)*2)) +
+    scico::scale_fill_scico_d(palette = colorscheme, direction = 1, name = variable, end = .85, begin = .3)  + 
     theme(axis.text.x = element_text(size = 15, angle = 90),
           axis.text.y = element_text(size = 15),
           axis.title.y = element_blank(),
           axis.line.y = element_blank(),
-          panel.grid.major.y = element_line(color = "grey80"),
-          legend.position = "None"
+          panel.grid.major.y = element_line(color = "grey80")
     )
   
-  
-    #get date in correct format
+  #get date in correct format
   #select date range
   #plot plot
 
@@ -191,16 +237,19 @@ ui <- fluidPage(#theme = shinytheme("flatly"),
                              choices = list("Profession", "Nationalität",
                                             "Perspektive_Identität", "Geschlecht"), selected = 1),
                  selectInput("p2_input2", h4("Kategorie auswählen"), 
-                             choices = list("Profession", "Nationalität",
-                                            "Perspektive_Identität", "Geschlecht"), selected = 1)),
+                             choices = list("Nationalität", "Profession", 
+                                            "Perspektive_Identität", "Geschlecht"), selected = 2)),
     mainPanel(h3(textOutput("t2")),
               plotlyOutput("p2"))
   ),
   
   sidebarLayout(
-    sidebarPanel(dateRangeInput("p3_input", h4("Zeitraum auswählen"), format = "yyyy-mm-dd", start  = "2023-10-07", min =  "2023-10-07")),
+    sidebarPanel(dateRangeInput("p3_input1", h4("Zeitraum auswählen"), format = "yyyy-mm-dd", start  = "2023-10-07", min =  "2023-10-07"),
+                 selectInput("p3_input2", h4("Kategorie auswählen"), 
+                             choices = list("Nationalität", "Profession", 
+                                            "Perspektive_Identität", "Geschlecht"), selected = 2)),
     mainPanel(h3(textOutput("t3")), 
-              plotOutput("p3"))
+              plotlyOutput("p3"))
   )
   
   
@@ -228,9 +277,16 @@ server <- function(input, output) {
   
   
   output$t3 = renderText(paste0("Anzahl der Runden pro Woche"))
-  output$p3 = renderPlot({
-    plot3(start= input$p3_input[1], end = input$p3_input[2],  df)
-  }, bg="transparent")
+  output$p3 = renderPlotly({
+    gg = plot3(start= input$p3_input1[1], end = input$p3_input1[2],  df, input$p3_input2)
+    ggplotly(gg, tooltip = "text")  %>%
+      layout(aspectratio = list(x = 2, y = 2),
+             xaxis = list(dtick = 2, rangemode = "tozero"), yaxis = list(dtick = 2, rangemode = "tozero")) %>%
+      config(displayModeBar = F)# Remove the "plotly" bar
+    
+    
+    
+  })
 }
 
 # Run the app ----
